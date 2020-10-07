81a71b100f3446395740f263d81ce055cb14425b
meghanamankar <59339656+meghanamankar@users.noreply.github.com>
2020-10-07 19:53:45 +0530

    Add files via upload

---
+++ meghana.ps1
@@ -0,0 +1,4 @@
+
+Get-CimInstance -ClassName Win32_ReliabilityRecords
+Get-EventLog -LogName Security -Newest 50
+Get-EventLog -ComputerName $Computer System -Source Microsoft-Windows-Winlogon ` | select $UserProperty,$TypeProperty,$TimeProperty
